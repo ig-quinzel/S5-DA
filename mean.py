89f70b9fb2636fd63a2488157dce2ee46b4b9b6b
Nandana K <128699882+ig-quinzel@users.noreply.github.com>
2024-11-10 12:28:04 +0530

    Update mean.py

--- mean.py
+++ mean.py
@@ -1,3 +1,5 @@
+import numpy as np
+import math
 n=int(input("length"))
 data=[]
 for i in range(n):
@@ -23,4 +25,31 @@ for i in data:
 maxx=max(lend.values())
 mode=[k for k,v in lend.items() if v==maxx]
 print(f'mode:{mode}')
-  
+if len(mode)==1:
+    print("unimode")
+elif len(mode)==2:
+     print("bimode")
+else:
+    print("trimode")
+q1=data[int(len(data)*0.25)]
+q2=median
+q3=data[int(len(data)*0.75)]
+iqr=q3-q1
+lo=q1-(1.5*iqr)
+ho=q3+(1.5*iqr)
+print(q1,q2,q3)
+print(lo,ho)
+new=[]
+for i in data:
+    if i>lo and i<ho:
+        new.append(i)
+print(new)
+print("max",max(new))
+print("min",min(new))
+vari=np.var(new)
+sd=math.sqrt(vari)
+print("variane:",vari,"sd:",sd)
+
+
+
+
